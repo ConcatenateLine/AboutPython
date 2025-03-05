74d373fae0ddc454dfb6c90fa29eb40b8d6cd65e
Concatenate Line <concatenateline@gmail.com>
2025-03-05 16:54:30 -0600

    fix: add static host

--- AboutDjango/mysite/settings.py
+++ AboutDjango/mysite/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
+STATIC_URL = STATIC_HOST + "/static/"
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
